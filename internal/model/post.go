44645a396b1b153139269569e57bc790efc58245
fikrify <hello@fikrify.com>
2026-05-13 15:42:56 +0700

    change json response to snake case

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,15 +1,13 @@
 package model
 
-import (
-	"time"
-)
+import "time"
 
 type Post struct {
-	ID        uint `gorm:"primaryKey"`
-	Title     string
-	Content   string
-	Category  string
-	Tags      []string `gorm:"serializer:json"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Category  string    `json:"category"`
+	Tags      []string  `json:"tags" gorm:"serializer:json"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
